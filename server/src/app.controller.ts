197f1f8036d2f3bdd42f3f666b2f51d797f3d9e4
Adarsh <ashu2100ag@gmail.com>
2026-03-20 23:30:52 +0530

    fixed forbidden error

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SkipResponseInterceptor } from './decorators/skip-success-interceptor.decorator';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   @SkipResponseInterceptor()
   Health(): string {
     return this.appService.health();
