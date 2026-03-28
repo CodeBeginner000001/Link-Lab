c2b6f38aba543548845ea13f46a8a3f03e52e169
Adarsh <ashu2100ag@gmail.com>
2026-03-29 03:46:40 +0530

    added env

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,1 +1,2 @@
-export const BACKEND_API_URL_ENV = process.env.BACKEND_API_URL || "http://localhost:4000/v1"
+export const BACKEND_API_URL_ENV =
+  process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:4000/v1";
