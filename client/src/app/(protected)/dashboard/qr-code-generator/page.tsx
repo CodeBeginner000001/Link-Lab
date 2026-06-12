b2fcae8abc216114c5521e50cc577ede04917171
Adarsh goyal <ashu2100ag@gmail.com>
2026-06-12 23:30:00 +0530

    qr code generator

---
+++ client/src/app/(protected)/dashboard/qr-code-generator/page.tsx
@@ -0,0 +1,12 @@
+import QrCodeGenerator from "@/modules/dashboard/pages/QrCodeGenerator"
+
+
+const page = () => {
+  return (
+    <>
+    <QrCodeGenerator/>
+    </>
+  )
+}
+
+export default page
---
+++ client/src/modules/dashboard/pages/QrCodeGenerator.tsx
@@ -0,0 +1,4 @@
+export default function QrCodeGenerator() {
+  // return <OneTimeLinkClient />;
+  return <>QrCodeGenerator</>;
+}
