412602e6f4d6e980d21daf5643aea5ef9e12cb8d
Adarsh goyal <ashu2100ag@gmail.com>
2026-06-16 23:44:48 +0530

    server deployment check-1

--- server/src/modules/features/barcodeGenerator/barcode-generator.controller.ts
+++ server/src/modules/features/barcodeGenerator/barcode-generator.controller.ts
@@ -53,6 +53,7 @@ export class BarcodeGeneratorController {
   getSummary(@Req() req: AuthenticatedRequest) {
     return this.barcodeService.getSummary(this.getUser(req));
   }
+
   @Get('analytics/activity')
   getActivity(
     @Query() query: GetBarcodeActivityDto,
@@ -60,10 +61,12 @@ export class BarcodeGeneratorController {
   ) {
     return this.barcodeService.getActivity(this.getUser(req), query);
   }
+
   @Delete(':id')
   delete(@Param('id') id: string, @Req() req: AuthenticatedRequest) {
     return this.barcodeService.delete(this.getUser(req), id);
   }
+
   @Get(':id/preview')
   @SkipResponseInterceptor()
   async preview(
